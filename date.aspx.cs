b1dce77c2fa83857f1605045640a5f5517746457
JDPATEL46 <42859899+JDPATEL46@users.noreply.github.com>
2019-01-20 21:46:49 +0530

    dir 2
    dir 2 complete

---
+++ date.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class date : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Calendar1.Visible = true;
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        TextBox1.Text = Calendar1.SelectedDate.ToString("dd - MMMM - yyyy");
+    }
+}
